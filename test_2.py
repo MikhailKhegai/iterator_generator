4b230cd58fe4edbfa6eee8d3762049fd0d8e7c7a
Mikhail Khegai <khegai200529@gmail.com>
2024-05-18 21:28:05 +0300

    init commit

---
+++ test_2.py
@@ -0,0 +1,30 @@
+import types
+
+
+def flat_generator(list_of_lists):
+    for l in list_of_lists:
+        for variable in l:
+            yield variable
+
+
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__test_2__':
+    test_2()
